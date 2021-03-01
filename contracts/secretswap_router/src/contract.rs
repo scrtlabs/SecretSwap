9416da5bd0e9690d5c50dc67cdbae891c2a0a0a7
Assaf Morami <assaf.morami@gmail.com>
2021-03-01 21:25:42 +0200

    router: first hop + misc functions

--- contracts/secretswap_router/src/contract.rs
+++ contracts/secretswap_router/src/contract.rs
@@ -1,68 +1,194 @@
 use cosmwasm_std::{
-    debug_print, from_binary, to_binary, Api, Binary, Env, Extern, HandleResponse, HandleResult,
-    HumanAddr, InitResponse, Querier, StdError, StdResult, Storage,
+    debug_print, from_binary, to_binary, Api, Binary, CosmosMsg, Env, Extern, HandleResponse,
+    HandleResult, HumanAddr, InitResponse, Querier, StdError, StdResult, Storage, WasmMsg,
 };
 use secret_toolkit::snip20;
+use HandleMsg::RecoverFunds;
 
-use crate::msg::{HandleMsg, InitMsg, QueryMsg, Route};
+use crate::{
+    msg::{HandleMsg, Hop, InitMsg, QueryMsg, Route, Swap},
+    state::{
+        delete_route_state, read_owner, read_route_state, read_tokens, store_owner,
+        store_route_state, store_tokens, RouteState,
+    },
+};
 
 pub fn init<S: Storage, A: Api, Q: Querier>(
-    _deps: &mut Extern<S, A, Q>,
-    _env: Env,
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
     _msg: InitMsg,
 ) -> StdResult<InitResponse> {
+    store_owner(&mut deps.storage, &env.message.sender)?;
+    store_tokens(&mut deps.storage, &vec![])?;
+
     Ok(InitResponse::default())
 }
 
 pub fn handle<S: Storage, A: Api, Q: Querier>(
-    _deps: &mut Extern<S, A, Q>,
+    deps: &mut Extern<S, A, Q>,
     env: Env,
     msg: HandleMsg,
 ) -> StdResult<HandleResponse> {
     match msg {
-        HandleMsg::Receive { from, msg, amount } => {
-            if let Some(bin_msg) = msg {
-                match from_binary(&bin_msg)? {
-                    Route { hops, to } => {
-                        // TODO
-                        // 1. save the remaining route to state (e.g. if the route is X/Y -> Y/Z -> Z->W then save Y/Z -> Z/W to state)
-                        // 2. send `amount` X to pair X/Y
-                    }
-                    _ => { /* TODO error  */ }
-                }
-            } else {
-                // TODO
-                // 1. load route from state (Y/Z -> Z/W)
-                // 2. save the remaining route to state (Z/W)
-                // 3. send `amount` Y to pair Y/Z
+        HandleMsg::Receive {
+            from,
+            msg: Some(msg),
+            amount,
+        } => {
+            // This is the first msg from the user, with the entire route details
+            // 1. save the remaining route to state (e.g. if the route is X/Y -> Y/Z -> Z->W then save Y/Z -> Z/W to state)
+            // 2. send `amount` X to pair X/Y
+            // 3. call FinalizeRoute to make sure everything went ok, otherwise revert the tx
 
-                // 1'. load route from state (Z/W)
-                // 2'. this is the last hop so delete the entire route state
-                // 3'. send `amount` Z to pair Z/W with recepient `to`
+            let Route { hops, to } = from_binary(&msg)?;
+
+            if hops.len() <= 1 {
+                return Err(StdError::generic_err("route must be at least 2 hops"));
             }
+
+            let first_hop: Hop = hops[0].clone();
+
+            if env.message.sender != first_hop.from_token.address {
+                return Err(StdError::generic_err(
+                    "route can only be initiated by sending here the token of the first hop",
+                ));
+            }
+
+            let remaining_hops: Vec<Hop> = hops[1..].to_vec();
+
+            store_route_state(
+                &mut deps.storage,
+                &RouteState {
+                    is_done: false,
+                    remaining_route: Route {
+                        hops: remaining_hops,
+                        to,
+                    },
+                },
+            )?;
+
+            Ok(HandleResponse {
+                messages: vec![
+                    snip20::send_msg(
+                        first_hop.pair_address,
+                        amount,
+                        Some(to_binary(&Swap::Swap {
+                            expected_return: None,
+                            to: Some(env.contract.address.clone()),
+                        })?),
+                        None,
+                        256,
+                        first_hop.from_token.code_hash,
+                        first_hop.from_token.address,
+                    )?,
+                    CosmosMsg::Wasm(WasmMsg::Execute {
+                        contract_addr: env.contract.address.clone(),
+                        callback_code_hash: env.contract_code_hash.clone(),
+                        msg: to_binary(&HandleMsg::FinalizeRoute {})?,
+                        send: vec![],
+                    }),
+                ],
+                log: vec![],
+                data: None,
+            })
         }
-        HandleMsg::RegisterRootTokens { tokens } => {
+        HandleMsg::Receive {
+            from,
+            msg: None,
+            amount,
+        } => {
+            // TODO
+            // env.msg.sender is the sending token
+
+            // 1. load route from state (Y/Z -> Z/W)
+            // 2. save the remaining route to state (Z/W)
+            // 3. send `amount` Y to pair Y/Z
+
+            // 1'. load route from state (Z/W)
+            // 2'. this is the last hop so delete the entire route state
+            // 3'. send `amount` Z to pair Z/W with recepient `to`
+            Ok(HandleResponse::default())
+        }
+        HandleMsg::FinalizeRoute {} => match read_route_state(&deps.storage)? {
+            Some(route_state) => {
+                if !route_state.is_done {
+                    return Err(StdError::generic_err("route is not done"));
+                }
+                delete_route_state(&mut deps.storage);
+
+                Ok(HandleResponse::default())
+            }
+            None => Err(StdError::generic_err("no route to finalize")),
+        },
+        HandleMsg::RegisterTokens { tokens } => {
+            let mut registered_tokens = read_tokens(&deps.storage)?;
             let mut msgs = vec![];
 
             for token in tokens {
+                if registered_tokens.contains(&token.address) {
+                    continue;
+                }
+                registered_tokens.push(token.address.clone());
+
                 msgs.push(snip20::register_receive_msg(
                     env.contract_code_hash.clone(),
                     None,
                     256,
                     token.code_hash.clone(),
                     token.address.clone(),
-                )?)
-                // TODO add token_address to list of registered tokens
+                )?);
+                msgs.push(snip20::set_viewing_key_msg(
+                    String::from("SecretSwap Router"),
+                    None,
+                    256,
+                    token.code_hash.clone(),
+                    token.address.clone(),
+                )?);
             }
 
-            return Ok(HandleResponse {
+            store_tokens(&mut deps.storage, &registered_tokens)?;
+
+            Ok(HandleResponse {
                 messages: msgs,
                 log: vec![],
                 data: None,
-            });
+            })
         }
-    };
-    Ok(HandleResponse::default())
+        RecoverFunds {
+            token,
+            amount,
+            to,
+            snip20_send_msg,
+        } => {
+            let owner = read_owner(&deps.storage)?;
+            if owner != env.message.sender {
+                return Err(StdError::unauthorized());
+            }
+
+            Ok(HandleResponse {
+                messages: vec![snip20::send_msg(
+                    to,
+                    amount,
+                    snip20_send_msg,
+                    None,
+                    256,
+                    token.code_hash,
+                    token.address,
+                )?],
+                log: vec![],
+                data: None,
+            })
+        }
+        HandleMsg::ChangeOwner { new_owner } => {
+            let current_owner = read_owner(&deps.storage)?;
+            if current_owner != env.message.sender {
+                return Err(StdError::unauthorized());
+            }
+
+            store_owner(&mut deps.storage, &new_owner)?;
+            Ok(HandleResponse::default())
+        }
+    }
 }
 
 pub fn query<S: Storage, A: Api, Q: Querier>(
--- contracts/secretswap_router/src/msg.rs
+++ contracts/secretswap_router/src/msg.rs
@@ -1,6 +1,5 @@
 use cosmwasm_std::{Binary, HumanAddr, Uint128};
 use schemars::JsonSchema;
-use secretswap::AssetInfo;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -8,15 +7,16 @@ pub struct InitMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Hop {
-    pair: HumanAddr,
-    from: AssetInfo,
-    expected_return: Option<Uint128>,
+    pub from_token: Token,
+    pub pair_address: HumanAddr,
+    pub pair_code_hash: String,
+    pub expected_return: Option<Uint128>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Route {
     pub hops: Vec<Hop>,
-    pub to: Option<HumanAddr>,
+    pub to: HumanAddr,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -33,8 +33,18 @@ pub enum HandleMsg {
         msg: Option<Binary>,
         amount: Uint128,
     },
-    RegisterRootTokens {
+    FinalizeRoute {},
+    RegisterTokens {
         tokens: Vec<Token>,
+    },
+    RecoverFunds {
+        token: Token,
+        amount: Uint128,
+        to: HumanAddr,
+        snip20_send_msg: Option<Binary>,
+    },
+    ChangeOwner {
+        new_owner: HumanAddr,
     },
 }
 
@@ -42,4 +52,13 @@ pub enum HandleMsg {
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     SupportedRootTokens {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum Swap {
+    Swap {
+        expected_return: Option<Uint128>,
+        to: Option<HumanAddr>,
+    },
 }
